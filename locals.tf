34349d2f9fe7ce7ad297d00a5e005c54733f67b6
AbhishekPanta8 <abhipantdev8@gmail.com>
2024-12-26 14:30:24 -0500

    temp fix for terraform duplicacy error

--- locals.tf
+++ locals.tf
@@ -1,21 +1,21 @@
 //we need repo names to start.
 locals {
   repos={
-    "fem-eci-terraform-tfe" = {
-      description = "This is a repo for automation for Terraform Enterprise"
-      gitignore_template = "Terraform"
-      name = "fem-eci-terraform-tfe"
-      topics = ["terraform", "automation"]
-      visibility = "public"
+    # "fem-eci-terraform-tfe" = {
+    #   description = "This is a repo for automation for Terraform Enterprise"
+    #   gitignore_template = "Terraform"
+    #   name = "fem-eci-terraform-tfe"
+    #   topics = ["terraform", "automation"]
+    #   visibility = "public"
 
-    }
-    "fem-eci-terraform-github" ={
-      description = "This is a repo for automation for GitHub"
-      gitignore_template = "Terraform"
-      name = "fem-eci-terraform-github"
-      topics = ["terraform", "automation"]
-      visibility = "public"
-    }
+    # }
+    # "fem-eci-terraform-github" ={
+    #   description = "This is a repo for automation for GitHub"
+    #   gitignore_template = "Terraform"
+    #   name = "fem-eci-terraform-github"
+    #   topics = ["terraform", "automation"]
+    #   visibility = "public"
+    # }
 
     "fem-eci-terraform-aws-network" = {
       description        = "Automation for aws networks"
